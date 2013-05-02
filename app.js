fdb90e1fcc531fb65e72eab72742ac790124a50e
Alex Kotliarskyi <alex.frantic@gmail.com>
2013-05-02 15:29:43 +0100

    Serve static content with express.js

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-var io = require('socket.io').listen(8080);
+var app = require('express')()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server);
+
+server.listen(8080);
+
 var spaceSocket = null;
 var clients = {};
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/view.html');
+});
+
+app.get('/control', function (req, res) {
+  res.sendfile(__dirname + '/control.html');
+});
 
 io.sockets.on('connection', function (socket) {
 
