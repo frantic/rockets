6cd71289ec8ed8e6c79c5712397d04faea53be05
Alex Kotliarskyi <alex.frantic@gmail.com>
2013-05-03 13:09:57 +0100

    Allow port configuration via ENV

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var app = require('express')()
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server);
 
-server.listen(8080);
+server.listen(process.env.PORT || 8080);
 
 var spaceSocket = null;
 var clients = {};
